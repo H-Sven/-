d8d45375891ea1ad6b3b885e32769230169abe02
siwen <623746556@qq.com>
2018-11-08 17:39:20 +0800

    删除echarts

--- src/main.js
+++ src/main.js
@@ -2,10 +2,8 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
-import VeLine from 'v-charts/lib/line.common';
 import { post, get} from "./assets/js/http";
 import uTils from "./assets/js/utils";
-Vue.component(VeLine.name, VeLine)
 
 Vue.config.productionTip = false;
 Vue.prototype.$post = post;
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,6 @@
 <template>
   <div class="home">
     <img src="../assets/images/logo.png" alt="" srcset="">
-    <ve-line :grid="grid" :data="chartData" :settings="chartSettings" v-show="false"></ve-line>
   </div>
 </template>
 
@@ -14,38 +13,11 @@ export default {
   },
   data(){
     return {
-      chartData: {
-        columns: ['日期', '销售额-1季度'],
-        rows: [
-          { '日期': '1', '销售额-1季度': 1523 },
-          { '日期': '2', '销售额-1季度': 1223 },
-          { '日期': '3', '销售额-1季度': 2123 },
-          { '日期': '4', '销售额-1季度': 4123 },
-          { '日期': '5', '销售额-1季度': 3123 },
-          { '日期': '6', '销售额-1季度': 7123 }
-        ]
-      }
+      
     }
   },
   created() {
-    this.chartSettings = {
-      axisVisible: false,
-      scale: [true],
-      area: true
-    }
-    this.grid = {
-      left: 0,
-      bottom: 0,
-      top: 30, // here
-      containLabel: false
-    }
-    var self = this
-    setInterval(function () {
-      self.chartData.rows.push({
-        '日期': self.chartData.rows.length,
-        '销售额-1季度': ~~(Math.random() * 10000)
-      })
-    }, 1000)
+    
   },
   mounted () {
     this.$get('/user/getUserInfo',{}).then(res=>{
