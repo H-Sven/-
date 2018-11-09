9d157411d58d515a18a320f92528a2e4d077bd62
siwen <623746556@qq.com>
2018-11-09 15:24:39 +0800

    添加elementUI

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,21 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <div>
-      <p>
-        If Element is successfully added to this project, you'll see an
-        <code v-text="'<el-button>'"></code>
-        below
-      </p>
-      <el-button>el-button</el-button>
-    </div>
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'app',
-  components: {
-    HelloWorld
+  data () {
+    return {
+      value2:false,
+    }
   }
 }
 </script>
 
-<style>
+<style style="less">
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
--- src/router.js
+++ src/router.js
@@ -7,6 +7,6 @@ Vue.use(Router);
 export default new Router({
   routes: [
     {path: "/",name: "home",component: Home},
-    {path: "/about",name: "about",component: () =>import(/* webpackChunkName: "about" */ "./views/About.vue")}
+    // {path: "/about",name: "about",component: () =>import(/* webpackChunkName: "about" */ "./views/About.vue")}
   ]
 });
